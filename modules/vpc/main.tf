8c87f1eaa0d73cbc139507addfd614777d9b70af
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-03 23:42:38 +0530

    Terraform Code Updated

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -14,3 +14,38 @@ resource "aws_subnet" "public" {
 }
 
 # Define web, app, and db subnets similarly
+# Web Subnets
+resource "aws_subnet" "web" {
+  count             = length(var.web_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.web_subnets[count.index]
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "web-subnet-${count.index}"
+  }
+}
+
+# Application Subnets
+resource "aws_subnet" "app" {
+  count             = length(var.app_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.app_subnets[count.index]
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "app-subnet-${count.index}"
+  }
+}
+
+# Database Subnets
+resource "aws_subnet" "db" {
+  count             = length(var.db_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.db_subnets[count.index]
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "db-subnet-${count.index}"
+  }
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,7 +1,7 @@
 ## modules/vpc/variables.tf
 variable "cidr" {}
 variable "public_subnets" { type = list(string) }
-# variable "web_subnets" { type = list(string) }
-# variable "app_subnets" { type = list(string) }
-# variable "db_subnets" { type = list(string) }
+variable "web_subnets" { type = list(string) }
+variable "app_subnets" { type = list(string) }
+variable "db_subnets" { type = list(string) }
 variable "azs" { type = list(string) }
