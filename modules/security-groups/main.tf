8b682729d30ac9d8880ef3093ec304155a1e37d9
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-04 10:23:23 +0530

    Terraform Code Updated

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -68,6 +68,7 @@ resource "aws_security_group" "bastion" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.bastion.id]
   }
 
   egress {
